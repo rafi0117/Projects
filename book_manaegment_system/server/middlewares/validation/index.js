00eaebd1c7229006d2b55348df18d6ca27fb5731
rafi0117 <mohdshabaaz786@gmail.com>
2022-09-29 11:47:19 +0530

    routes have been updated, react pages are completed for login and register

--- book_manaegment_system/server/middlewares/validation/index.js
+++ book_manaegment_system/server/middlewares/validation/index.js
@@ -19,6 +19,24 @@ function userRegisterValidatorRules() {
     ]
 }
 
+function addbookvalidations() {
+    return [
+body("title","Title is Required").isString({min:2}),
+body("Author","Author is Required").isString({min:6}),
+body("coverImagrUrl","Image URL is Required").isURL(),
+body("publisher","Publisher Name is Required").isString({min:2}),
+body("synopsis","Synopsis is Required").isString({min:2}),
+body("PageCount","Number is Required").notEmpty({min:2}),
+    ]
+}
+
+function loginValidation() {
+    return [
+        body('email', "Email Is Required").isEmail(),
+        body('password', "Passowrd is Required").notEmpty()
+    ]
+}
+
 function errorMiddleware(req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -26,9 +44,10 @@ function errorMiddleware(req, res, next) {
     }
     return next();
 }
-function userLoginValidatorRules() {
-    return [body("password", "Password is Required").notEmpty(),
-    body("email", "Email is Required").isEmail()]
-}
 
-export { userLoginValidatorRules,userRegisterValidatorRules, errorMiddleware }
+
+
+export {
+    userRegisterValidatorRules,
+    errorMiddleware,loginValidation,addbookvalidations
+}
